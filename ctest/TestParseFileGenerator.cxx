410aadc05e57fd41e7824dd99fe47cb11b9739ab
giorgi.page <andreagiorgino1@gmail.com>
2026-06-26 19:46:52 +0200

    test parse file generator implemented

---
+++ ctest/TestParseFileGenerator.cxx
@@ -0,0 +1,91 @@
+#include <array>
+#include <iostream>
+
+#include "libtokenizer/tokenizer.hxx"
+
+static constexpr auto testFilePath { "TestFiles/TestParseFileGenerator.txt" };
+static constexpr std::array<libtokenizer::tokenizer::token, 51> tokensResult {{
+    { 0,   "#", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 1,   "include", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 8,   " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 9,   "<", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 10,  "print", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 15,  ">", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 16,  "\n", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 17,  "\n", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 18,  "auto", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 22,  " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 23,  "main", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 27,  "(", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 28,  "int", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 31,  " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 32,  "argc", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 36,  ",", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 37,  " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 38,  "char", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 42,  "*", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 43,  "*", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 44,  " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 45,  "argv", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 49,  ")", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 50,  " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 51,  "-", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 52,  ">", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 53,  " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 54,  "int", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 57,  " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 58,  "{", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 59,  "\n", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 60,  "    ", libtokenizer::tokenizer::token_t::SPACE },
+    { 64,  "std", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 67,  ":", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 68,  ":", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 69,  "println", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 76,  "(", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 77,  "\"Hello, world!\"", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 92,  ")", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 93,  ";", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 94,  "\n", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 95,  "    ", libtokenizer::tokenizer::token_t::SPACE },
+    { 99,  "return", libtokenizer::tokenizer::token_t::ALPHANUMERIC },
+    { 105, " ", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 106, "0", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 107, ";", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 108, "\n", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 109, "}", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 110, "\n", libtokenizer::tokenizer::token_t::SYMBOL },
+    { 111, "\x{ff}", libtokenizer::tokenizer::token_t::END_OF_FILE },
+    { 111, std::string { (char)EOF }, libtokenizer::tokenizer::token_t::END_OF_FILE },
+}};
+
+auto TestParseFileGenerator(int, char**) -> int {
+    libtokenizer::tokenizer t { testFilePath };
+
+    size_t i {};
+    for (const auto& token : t.tokens()) {
+        if (i >= tokensResult.size()) {
+            std::println(std::cerr,
+                    "-- Token index out of bounds: accessing index {} "
+                    "out of {} expected tokens.",
+                    i, tokensResult.size());
+            return 1;
+        }
+
+        if (const auto& tokenResult { tokensResult[i++] };
+                token.position != tokenResult.position
+                || token.literal != tokenResult.literal
+                || token.type != tokenResult.type) {
+            std::println(std::cerr,
+                    "-- Token not matching the expected result: "
+                    "\"{{ {}, {:?}, {} }}\" expected, got \"{{ {}, {:?}, {} }}\" instead.",
+                    tokenResult.position, tokenResult.literal,
+                    libtokenizer::tokenizer::get_token_t_name(tokenResult.type),
+                    token.position, token.literal,
+                    libtokenizer::tokenizer::get_token_t_name(token.type));
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
