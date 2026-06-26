6f154a92c69a5c69be3d03c9809a90a80b0126b1
giorgi.page <andreagiorgino1@gmail.com>
2026-06-26 19:42:58 +0200

    libtokenizer::errors::file_error deleted in favor of libparser::errors::file_error

--- ctest/TestOpenFileErrorEmptyString.cxx
+++ ctest/TestOpenFileErrorEmptyString.cxx
@@ -1,14 +1,14 @@
 #include <iostream>
 
 #include "libtokenizer/tokenizer.hxx"
-#include "libtokenizer/errors/file_error.hxx"
+#include "libparser/errors/file_error.hxx"
 
 static constexpr auto testFilePath { "" };
 
 auto TestOpenFileErrorEmptyString(int, char**) -> int {
     try {
         libtokenizer::tokenizer t { testFilePath };
-    } catch (const libtokenizer::errors::file_error& err) {
+    } catch (const libparser::errors::file_error& err) {
         return 0;
     } catch (const std::exception& err){
         std::cerr << err.what() << std::endl;
