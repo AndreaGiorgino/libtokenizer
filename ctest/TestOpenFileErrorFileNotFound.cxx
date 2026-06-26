899870fd24b83ad820e937dd8222bbca7e822d4c
giorgi.page <andreagiorgino1@gmail.com>
2026-06-26 15:21:55 +0200

    open file testing implemented

---
+++ ctest/TestOpenFileErrorFileNotFound.cxx
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "libtokenizer/tokenizer.hxx"
+#include "libtokenizer/errors/file_error.hxx"
+
+static constexpr auto testFilePath { "TestFiles/TestFileNotFound.txt" };
+
+auto TestOpenFileErrorFileNotFound(int, char**) -> int {
+    try {
+        libtokenizer::tokenizer t { testFilePath };
+    } catch (const libtokenizer::errors::file_error& err) {
+        return 0;
+    } catch (const std::exception& err){
+        std::cerr << err.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
