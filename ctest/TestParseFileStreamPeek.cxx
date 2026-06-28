d0f0e0936869b5171f555d07d11c7dc48619fc83
giorgi.page <andreagiorgino1@gmail.com>
2026-06-28 19:35:06 +0200

    test stream peek implemented

---
+++ ctest/TestParseFileStreamPeek.cxx
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include "libtokenizer/tokenizer.hxx"
+
+static constexpr auto testFilePath { "TestFiles/TestParseFileStream.txt" };
+static constexpr libtokenizer::tokenizer::token tokenResult {
+    0, "#", libtokenizer::tokenizer::token_t::HASH
+};
+
+auto TestParseFileStreamPeek(int, char**) -> int {
+    libtokenizer::tokenizer t { testFilePath };
+
+    const auto lambda {
+        [&](void) -> int {
+            const auto token { t.peek() };
+            if (token.position != tokenResult.position
+                    || token.literal != tokenResult.literal
+                    || token.type != tokenResult.type) {
+                std::println(std::cerr,
+                        "-- Token not matching the expected result: "
+                        "\"{{ {}, {:?}, {} }}\" expected, got "
+                        "\"{{ {}, {:?}, {} }}\" instead.",
+                        tokenResult.position, tokenResult.literal,
+                        libtokenizer::tokenizer::get_token_t_name(tokenResult.type),
+                        token.position, token.literal,
+                        libtokenizer::tokenizer::get_token_t_name(token.type));
+                return 1;
+            }
+            return 0;
+        }
+    };
+
+    if (const auto ret { lambda() };
+            ret != 0)
+        return ret;
+    if (const auto ret { lambda() };
+            ret != 0)
+        return ret;
+
+    return 0;
+}
