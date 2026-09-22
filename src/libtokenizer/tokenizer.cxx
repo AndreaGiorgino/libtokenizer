20240225bafde695ed11e1e0c64cad910cc9b11b
AndreaGiorgino <andreagiorgino1@gmail.com>
2026-09-22 18:39:28 +0200

    feat: parsing utilities declared

--- src/libtokenizer/tokenizer.cxx
+++ src/libtokenizer/tokenizer.cxx
@@ -1,6 +1,18 @@
+#include <cassert>
 #include <libtokenizer/tokenizer.hxx>
+#include <utility>
 
-Tokenizer::Tokenizer(std::istream& is, bool collapseStrings)
+// Declarations ///////////////////////////////////////////////////////////////
+
+[[nodiscard]] auto tryParseIdentifier(std::istream& is,
+                                      uint8_t options) noexcept
+    -> Tokenizer::Token;
+
+[[nodiscard]] auto tryParseString(std::istream& is, uint8_t options) noexcept
+    -> Tokenizer::Token;
+
+[[nodiscard]] auto tryParseNumeric(std::istream& is, uint8_t options) noexcept
+    -> Tokenizer::Token;
     : _is(is),
       _collapseStrings(collapseStrings) {
     if (!is)
