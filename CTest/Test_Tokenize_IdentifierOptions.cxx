791284ab7152040329a8acd7620de2f93abf2e76
AndreaGiorgino <andreagiorgino1@gmail.com>
2026-09-25 15:10:24 +0200

    update: Test_Tokenize_IdentifierOptions

--- CTest/Test_Tokenize_IdentifierOptions.cxx
+++ CTest/Test_Tokenize_IdentifierOptions.cxx
@@ -3,22 +3,49 @@
 #include "utility.hxx"
 
 auto Test_Tokenize_IdentifierOptions(int, char**) -> int {
-    std::stringstream ss {"some_var  1_1"};
-    Tokenizer tokenizer {ss, {}};
+    {
+        // no options
+        std::stringstream ss {"some-var  1_1"};
+        Tokenizer tokenizer {ss, {}};
 
-    expectToken(tokenizer.get(), "some", Tokenizer::TokenType::Identifier,
-                __LINE__);
-    expectToken(tokenizer.get(), "_", Tokenizer::TokenType::Symbol, __LINE__);
-    expectToken(tokenizer.get(), "var", Tokenizer::TokenType::Identifier,
-                __LINE__);
-    expectToken(tokenizer.get(), " ", Tokenizer::TokenType::Space, __LINE__);
-    expectToken(tokenizer.get(), " ", Tokenizer::TokenType::Space, __LINE__);
-    expectToken(tokenizer.get(), "1", Tokenizer::TokenType::NumericInt,
-                __LINE__);
-    expectToken(tokenizer.get(), "_", Tokenizer::TokenType::Symbol, __LINE__);
-    expectToken(tokenizer.get(), "1", Tokenizer::TokenType::NumericInt,
-                __LINE__);
-    expectToken(tokenizer.get(), "", Tokenizer::TokenType::Eos, __LINE__);
+        expectToken(tokenizer.get(), "some", Tokenizer::TokenType::Identifier,
+                    __LINE__);
+        expectToken(tokenizer.get(), "-", Tokenizer::TokenType::Symbol,
+                    __LINE__);
+        expectToken(tokenizer.get(), "var", Tokenizer::TokenType::Identifier,
+                    __LINE__);
+        expectToken(tokenizer.get(), " ", Tokenizer::TokenType::Space,
+                    __LINE__);
+        expectToken(tokenizer.get(), " ", Tokenizer::TokenType::Space,
+                    __LINE__);
+        expectToken(tokenizer.get(), "1", Tokenizer::TokenType::NumericInt,
+                    __LINE__);
+        expectToken(tokenizer.get(), "_", Tokenizer::TokenType::Symbol,
+                    __LINE__);
+        expectToken(tokenizer.get(), "1", Tokenizer::TokenType::NumericInt,
+                    __LINE__);
+        expectToken(tokenizer.get(), "", Tokenizer::TokenType::Eos, __LINE__);
+    }
+
+    {
+        std::stringstream ss {"some-var  1_1"};
+        Tokenizer tokenizer {ss, Tokenizer::OptionsSet {}.set(
+                                     Tokenizer::Options::AllowDashIdentifier)};
+
+        expectToken(tokenizer.get(), "some-var",
+                    Tokenizer::TokenType::Identifier, __LINE__);
+        expectToken(tokenizer.get(), " ", Tokenizer::TokenType::Space,
+                    __LINE__);
+        expectToken(tokenizer.get(), " ", Tokenizer::TokenType::Space,
+                    __LINE__);
+        expectToken(tokenizer.get(), "1", Tokenizer::TokenType::NumericInt,
+                    __LINE__);
+        expectToken(tokenizer.get(), "_", Tokenizer::TokenType::Symbol,
+                    __LINE__);
+        expectToken(tokenizer.get(), "1", Tokenizer::TokenType::NumericInt,
+                    __LINE__);
+        expectToken(tokenizer.get(), "", Tokenizer::TokenType::Eos, __LINE__);
+    }
 
     return 0;
 }
